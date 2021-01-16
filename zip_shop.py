abe0be9b18660a9d6172b05c076821fda5fc3e89
Sergey Segeda <segeda823@mail.ru>
2021-01-16 17:41:25 +0300

    Rename script

--- zip_shop.py
+++ zip_shop.py
@@ -4,32 +4,38 @@ import zipfile
 current_folder_name = os.path.basename(os.getcwd())
 archive_name = f'{current_folder_name}.zip'
 exclude_folders = (f'.{os.sep}.idea', f'.{os.sep}.git', f'.{os.sep}.vscode')
-exclude_files = (archive_name, os.path.basename(__file__))
+exclude_files = (archive_name, os.path.basename(__file__), '.gitignore')
 exclude_configs = (f'.{os.sep}config.php', f'.{os.sep}admin{os.sep}config.php')
 
 
-def zip_dir(path, zip_handler, exclude_fold, exclude_fl):
+def check_folder(exclude_folder, main_folder):
+    for folder in exclude_folder:
+        if folder in main_folder:
+            return True
+    return False
+
+
+def check_configs(file_full_path, exclude_configs_tuple):
+    for config_file in exclude_configs_tuple:
+        if file_full_path == config_file:
+            return True
+    return False
+
+
+def zip_dir(path, zip_handler, exclude_fold, exclude_fl, exclude_conf):
     path_len = len(path)
     for root, _, files in os.walk(path):
-        check_folder = False
-        for folder in exclude_fold:
-            if folder in root:
-                check_folder = True
-        if check_folder:
+        if check_folder(exclude_fold, root):
             continue
         for file in files:
             if file in exclude_fl:
                 continue
             file_path = os.path.join(root, file)
-            check_configs = False
-            for config_file in exclude_configs:
-                if file_path == config_file:
-                    check_configs = True
-            if check_configs:
+            if check_configs(file_path, exclude_conf):
                 continue
             zip_handler.write(file_path, file_path[path_len:])
 
 
 with zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED) as zip_fl:
-    zip_dir(f'.{os.sep}', zip_fl, exclude_folders, exclude_files)
+    zip_dir(f'.{os.sep}', zip_fl, exclude_folders, exclude_files, exclude_configs)
     zip_fl.close()
